f5e65d693355a34563b62196a70c8af5dbab29da
Ricardo dos Santos <ricardo.santos@zup.com.br>
2023-08-03 08:15:47 -0300

    Deletando pasta que subiu por engano

--- darkSideBooks/repository/LivrosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.catalisa.darkSideBooks.repository;
-
-import com.catalisa.darkSideBooks.model.LivroModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LivrosRepository extends JpaRepository<LivroModel, Long> { }
